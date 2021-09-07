7fa6b056428c3661ecb5c1ee5b51dcab4bc97019
Damyan Dimitrov <damqn_96@abv.bg>
2021-09-07 22:30:59 +0300

    Update Chat - Fix in groups visibility

--- Quizo/Hubs/ChatHub.cs
+++ Quizo/Hubs/ChatHub.cs
@@ -1,13 +1,58 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Quizo.Data;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Connections;
+using System.Collections.Generic;
+using System;
 
 namespace Quizo.Hubs
 {
 	public class ChatHub : Hub
 	{
-		public async Task Send(string name, string message)
+        private readonly QuizoDbContext db;
+		private static Dictionary<string, string> connectionsNgroup = new Dictionary<string, string>();
+
+
+		public ChatHub(QuizoDbContext db)
 		{
-			await Clients.All.SendAsync("broadcastMessage", name, message);
+			this.db = db;
+		}
+
+		public async Task Send(string message, string nameGroup)
+		{
+			string username = Context.User.Identity.Name;
+			if (connectionsNgroup.ContainsKey(Context.ConnectionId))
+			{
+				await Clients.Group(connectionsNgroup[Context.ConnectionId]).SendAsync("broadcastMessage", username, message);
+			}
+			//await Clients.Group(nameGroup).SendAsync("broadcastMessage", username, message);
+		}
+		public async Task JoinGroup(string groupId)
+		{
+			var group = db.Groups.Find(groupId);
+
+			if (connectionsNgroup.ContainsKey(Context.ConnectionId))
+			{
+				await Groups.RemoveFromGroupAsync(Context.ConnectionId, connectionsNgroup[Context.ConnectionId]);
+				connectionsNgroup.Remove(Context.ConnectionId);
+			}
+			connectionsNgroup.Add(Context.ConnectionId, group.Name);
+			await Groups.AddToGroupAsync(Context.ConnectionId, group.Name);
+		}
+
+		public async override Task OnConnectedAsync()
+        {
+			await base.OnConnectedAsync();
+        }
+
+		public override async Task OnDisconnectedAsync(Exception exception)
+		{
+			if (connectionsNgroup.ContainsKey(Context.ConnectionId))
+			{
+				await Groups.RemoveFromGroupAsync(Context.ConnectionId, connectionsNgroup[Context.ConnectionId]);
+				connectionsNgroup.Remove(Context.ConnectionId);
+			}
+			await base.OnDisconnectedAsync(exception);
 		}
 	}
 }
---
+++ Quizo/wwwroot/js/chat.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+	var messageInput = document.getElementById('message');
+	var id = document.getElementById('groupId');
+
+	messageInput.focus();
+
+	var route = '/chat';
+	var connection = new signalR.HubConnectionBuilder()
+		.withUrl(route)
+		.build();
+
+	connection.on('broadcastMessage', function (name, message) {
+		var encodedName = name;
+		var encodedMsg = message;
+
+		var liElement = document.createElement('li');
+		liElement.innerHTML = '<strong>' + encodedName + '</strong>:&nbsp;&nbsp;' + encodedMsg;
+		document.getElementById('discussion').appendChild(liElement);
+	});
+
+	connection.start()
+		.then(function () {
+			console.log('connection started');
+
+			connection
+				.invoke("JoinGroup", id.value)
+				.catch(err => console.error(err));
+
+			var groupName = document.getElementById("nameGroup");
+			document.getElementById('sendmessage').addEventListener('click', function (event) {
+				connection.invoke('send', messageInput.value, groupName.value);
+
+				messageInput.value = '';
+				messageInput.focus();
+				event.preventDefault();
+			});
+		})
+		.catch(error => {
+			console.error(error.message);
+		});
+});
